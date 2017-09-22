e94f2afdf14de706beabf3df754c6f1e2ef2cacd
Minu James <minujames@minus-mbp.gateway.pace.com>
2017-09-21 23:19:47 -0700

    adding sound effects and error checks

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# hangman-game
+# Safari Hangman
+This is a hangman game in Animal Safari theme. Animals names are the words to guess.
+The users can start playing game by pressing any letter.
+
+### Features
+
+* For every word, there are (word length + 5) guessing options
+* For every correct guess, animal image is shown with a victory sound.
+* For every lose, corresponding image and sounds are displayed
+
+
+## Built With
+* [JavaScript](https://www.javascript.com/)
+* [Bootstrap 3.3.6](http://bootstrapdocs.com/v3.3.6/docs/)
+* [CSS](https://www.w3schools.com/css/)
+
+## Deployment
+The portfolio website is accessible at https://minujames.github.io/hangman-game/
+
+## Author
+[Minu James](https://minujames.github.io/)
+
+## License
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,24 @@
+
+// Variables to access the DOM elements
 var winsText = document.getElementById("wins-count");
 var currentWordText = document.getElementById("current-word");
 var remainingGuessesText = document.getElementById("remaining-guesses");
 var lettersGuessedText = document.getElementById("letters-guessed");
+// var jungleImage = document.getElementById("jungle-img");
+var imageDiv = document.getElementById("img-div");
 
+// variables to hold the images/sound file urls
+var imgBaseUrl = "assets/images/";
+var tryAgainImgName = "try-again";
+var imageExtension = ".jpg";
+var winningSoundUrl = "assets/sounds/applause.mp3";
+var oopsSoundUrl = "assets/sounds/oops.mp3";
+
+// Main game object
 var hangMan = {
 
-  animalList: ['ALLIGATOR', 'BULLDOG', 'BUTTERFLY', 'CHEETAH', 'CHAMELEON', 'CHIMPANZEE', 'CROCODILE',
-              'DALMATIAN', 'DONKEY'],
+  animalList: ['BUTTERFLY', 'CHEETAH', 'CHAMELEON', 'CHIMPANZEE', 'CROCODILE',
+              'DALMATIAN', 'DONKEY', 'GIRAFFE', 'ZEBRA', 'ELEPHANT', 'PARROT'],
   
   currentWord: '',
 
@@ -18,11 +30,14 @@ var hangMan = {
   guessBuffer: 5,
   placeHolderCharacter: '_',
 
+  // Function to start the game 
   initialize: function() {
     this.reset();
     reWriteDocument();
   },
 
+  // Resets all variables to initial state except the win variable.
+  // This method is called after each win and lose to restart the game.
   reset: function(){
     this.lettersGuessed = [];
     this.currentWord = this.animalList[Math.floor(Math.random() * this.animalList.length)];
@@ -32,6 +47,8 @@ var hangMan = {
     console.log("current word: " + this.currentWord);
   },
 
+
+  // This method evaluates user's input on each keyup and takes appropriate steps. 
   evaluateUserInput: function(guessedLetter){
     if(this.lettersGuessed.indexOf(guessedLetter) == -1){
       if(this.currentWord.includes(guessedLetter)){
@@ -44,14 +61,18 @@ var hangMan = {
         }
         this.placeHolderWord = charArray.join('');
 
-        if(!(this.placeHolderWord.includes(this.placeHolderCharacter))) {
+        if(this.currentWord === this.placeHolderWord) {
             this.wins++;
+            renderAnimalImage();
+            playSound(winningSoundUrl);
             this.reset();
         }
       }
       else{
         this.remainingGuesses--;
         if(this.remainingGuesses === 0){
+          renderImage(tryAgainImgName);
+          playSound(oopsSoundUrl);
           this.reset();
         }
         else{
@@ -63,28 +84,53 @@ var hangMan = {
   }
 };
 
+// Game starting point
 hangMan.initialize();
 
+// Methid to update the DOM element
 function reWriteDocument(){
   winsText.textContent = hangMan.wins;
   currentWordText.textContent = hangMan.placeHolderWord.split('').join (" ");
   remainingGuessesText.textContent = hangMan.remainingGuesses;
   lettersGuessedText.textContent = hangMan.lettersGuessed.join(" ");
-  console.log("placeHolderWord: " + hangMan.placeHolderWord);
 }
 
-// This function is run whenever the user presses a key.
-document.onkeyup = function(event) {
+// Method to render image to DOM
+function renderAnimalImage(){
+  // All animal images are stored in the same name as the words in the animal list.
+  // Appending current word with the base url in order to get the full image path.
+  renderImage(hangMan.currentWord.toLowerCase());
+}
+
+function renderImage(imageName)
+{
+  var imgPath = imgBaseUrl + imageName + imageExtension;
+  var newImg = document.createElement("img");
+  newImg.setAttribute("src", imgPath);
+  newImg.setAttribute("class", "thumbnail animal-image");
+  imageDiv.innerHTML = "";
+  imageDiv.appendChild(newImg);
+}
+
+// Method to play sound
+function playSound(soundUrl){
+  var audio = new Audio(soundUrl);
+  audio.play();
+}
+
+// This method executes on each user input
+document.onkeyup = function(event){
+  
   // Determines which key was pressed.
   var guessedCharacter = event.key;
-  console.log("guessedCharacter: " + guessedCharacter);
+  console.log("user character: " + guessedCharacter);
 
-  // checks if the pressed key is an alphabet
+  // Checks if the pressed key is a letter
   if( guessedCharacter.length == 1 && (/[a-z]/i.test(guessedCharacter)))
   {
+    // Converting to uppercase. This is for an easy comparison with the computer generated words, 
+    // which are stored in upper case letters.
     var guessedLetter = guessedCharacter.toUpperCase();
-    console.log("guessedLetter: "+ guessedLetter);
-
     hangMan.evaluateUserInput(guessedLetter);
   }
 }
